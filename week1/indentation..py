e432d28ef1e2ac17db0a6e67b4e716731422aeeb
K Mahesh Naik <143249434+Mahesh85639@users.noreply.github.com>
2024-09-18 21:46:24 +0530

    Add files via upload

---
+++ week1/indentation..py
@@ -0,0 +1,16 @@
+
+ #purposely raise an indentation error and correct indentation
+a=float(input("enter the value of a: "))
+b=float(input("enter the value of b: "))
+if a<=b:
+  print("a is big") 
+else:
+  print("b is big")
+
+#correct code 
+a=float(input("enter the value of a: "))
+b=float(input("enter the value of b: "))
+if a<=b:
+    print("a is small") 
+else: 
+    print("b is big") 
