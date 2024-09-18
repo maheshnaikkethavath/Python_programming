defef0964b1ded407dcca63c64b4cf6d5b8087af
K Mahesh Naik <143249434+Mahesh85639@users.noreply.github.com>
2024-09-18 21:57:16 +0530

    Delete details.py

--- details.py
+++
@@ -1,8 +0,0 @@
-name=input("Enter the name of student: ")
-address=input("Enter the address of student: ")
-phone_no=input("Enter the phone number of student: ")
-email=input("Enter the email of a student: ")
-print("Name of the student is ",name)
-print("adddress of the student is ",address)
-print("email of the student is ",email)
-print("phone_no of the student is ",phone_no)
